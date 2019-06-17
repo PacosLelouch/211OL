fec6985d4596e46a43d386de410c37fe3b132657
JanHoChoi <435504339@qq.com>
2019-06-18 04:44:59 +0800

    update

---
+++ src/java_code/Response.java
@@ -0,0 +1,32 @@
+package java_code;
+
+public class Response {
+	
+	private Boolean status;
+	private String msg;
+	private Object data;
+	
+	public Boolean getStatus() {
+		return this.status;
+	}
+	
+	public String getMsg() {
+		return this.msg;
+	}
+
+	public Object getData() {
+		return this.data;
+	}
+	
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public void setData(Object data) {
+		this.data = data;
+	}
+}
